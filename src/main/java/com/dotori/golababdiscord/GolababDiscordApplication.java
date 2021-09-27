9c24c77e9c9fa0d370a08e9bdcd237c3ea6cc0b0
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2021-09-27 22:57:43 +0900

    Permission 에 따른 Role 지급 및 투표 채널 관련 커맨드 생성 AOP 오류 해결

--- src/main/java/com/dotori/golababdiscord/GolababDiscordApplication.java
+++ src/main/java/com/dotori/golababdiscord/GolababDiscordApplication.java
@@ -2,16 +2,15 @@ package com.dotori.golababdiscord;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.logging.LogLevel;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableAspectJAutoProxy
 @SpringBootApplication
 @EnableScheduling
+@EnableAspectJAutoProxy
 public class GolababDiscordApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GolababDiscordApplication.class, args);
 	}
-
 }
