c6a16e7a663b31a6626c467bab2096c57474092a
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2021-10-26 20:40:49 +0900

    [FIX] 국소 오류 수정 및 ApiCaller 에 ranking 관련 로직 추가

--- src/main/java/com/dotori/golababdiscord/domain/api/service/caller/ApiCaller.java
+++ src/main/java/com/dotori/golababdiscord/domain/api/service/caller/ApiCaller.java
@@ -3,10 +3,9 @@ package com.dotori.golababdiscord.domain.api.service.caller;
 import com.dotori.golababdiscord.domain.api.dto.RequestCollectedVoteDto;
 import com.dotori.golababdiscord.domain.api.dto.ResponseDayMenuDto;
 import com.dotori.golababdiscord.domain.api.dto.ResponseMealMenuDto;
-import com.dotori.golababdiscord.domain.api.service.LunchApiService;
-import com.dotori.golababdiscord.domain.api.service.TipticApiService;
-import com.dotori.golababdiscord.domain.api.service.VoteApiService;
+import com.dotori.golababdiscord.domain.api.service.*;
 import com.dotori.golababdiscord.domain.logger.annotation.ApiEntry;
+import com.dotori.golababdiscord.domain.ranking.dto.RequestRankingDto;
 import com.dotori.golababdiscord.domain.vote.enum_type.MealType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -15,10 +14,11 @@ import java.util.Date;
 
 @Component
 @RequiredArgsConstructor
-public class ApiCaller implements LunchApiService, TipticApiService, VoteApiService{
+public class ApiCaller implements LunchApiService, TipticApiService, VoteApiService, ViewerApiService {
     private final LunchApiService lunchApiService;
     private final VoteApiService voteApiService;
     private final TipticApiService tipticApiService;
+    private final ViewerApiService viewerApiService;
 
     @Override @ApiEntry(apiName = "lunch_api", actionName = "get_meals_today", request = Void.class, response = ResponseDayMenuDto.class)
     public ResponseDayMenuDto getMealsToday() {
@@ -45,5 +45,10 @@ public class ApiCaller implements LunchApiService, TipticApiService, VoteApiServ
     @Override @ApiEntry(apiName = "vote_api", actionName = "collect_total_vote_at_day", request = RequestCollectedVoteDto.class, response = Void.class)
     public void collectTotalVoteAtDay(RequestCollectedVoteDto result) {
         voteApiService.collectTotalVoteAtDay(result);
+    }
+
+    @Override @ApiEntry(apiName = "viewer_api", actionName = "menu_ranking", request = Void.class, response = RequestRankingDto.class)
+    public RequestRankingDto getRanking(int start, int end) {
+        return viewerApiService.getRanking(start, end);
     }
 }
