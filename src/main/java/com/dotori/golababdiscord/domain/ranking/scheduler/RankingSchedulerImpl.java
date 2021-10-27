23bf2f0117c741c0717abcb0bd835825f91dbede
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2021-10-27 19:08:20 +0900

    [FIX] 필요없는 Test 구문 제거

--- src/main/java/com/dotori/golababdiscord/domain/ranking/scheduler/RankingSchedulerImpl.java
+++ src/main/java/com/dotori/golababdiscord/domain/ranking/scheduler/RankingSchedulerImpl.java
@@ -3,10 +3,7 @@ package com.dotori.golababdiscord.domain.ranking.scheduler;
 import com.dotori.golababdiscord.domain.ranking.service.RankingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.scheduling.support.CronExpression;
 import org.springframework.stereotype.Component;
-
-import java.time.LocalDateTime;
 
 @Component
 @RequiredArgsConstructor
@@ -16,16 +13,5 @@ public class RankingSchedulerImpl implements RankingScheduler{
     @Override @Scheduled(cron = "0 0 12 14,28 * *") //매일 12시마다 팁틱 게시
     public void sendRanking() {
         rankingService.sendResult2Weeks();
-    }
-
-    public static void main(String[] args) {
-        String cronExpression = "0 0 12 14,28 * *";
-
-        LocalDateTime time = LocalDateTime.now();
-        while(true) {
-            LocalDateTime next = CronExpression.parse(cronExpression).next(time);
-            System.out.println(next);
-            time = next;
-        }
     }
 }
